7deec5ce40249dc17cef276099304040e68efd45
kesef-mario <tech@kesef.finance>
2021-05-16 22:02:02 +0530

    dist

---
+++ dist/components/Toggle/StyledToggle.d.ts
@@ -0,0 +1,8 @@
+export declare const Handle: import("styled-components").StyledComponent<"div", import("styled-components").DefaultTheme, {
+    checked: boolean | undefined;
+}, never>;
+export declare const Input: import("styled-components").StyledComponent<"input", import("styled-components").DefaultTheme, {}, never>;
+declare const StyledToggle: import("styled-components").StyledComponent<"div", import("styled-components").DefaultTheme, {
+    checked: boolean;
+}, never>;
+export default StyledToggle;
