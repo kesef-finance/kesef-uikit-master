62e8a215c5fae46ada175cbfdb865398c9840ccf
kesef-mario <tech@kesef.finance>
2021-05-16 23:28:41 +0530

    colors

--- dist/index.esm.js
+++ dist/index.esm.js
@@ -2695,21 +2695,22 @@ var ResetCSS = createGlobalStyle(templateObject_1 || (templateObject_1 = __makeT
 var templateObject_1;
 
 var baseColors = {
-    failure: "#ED4B9E",
-    primary: "#ffe184",
-    primaryBright: "#ffe184",
-    primaryDark: "#ffe184",
-    secondary: "#ffe184",
+    failure: "#00cc98",
+    primary: "##090b16",
+    primaryBright: "##090b16",
+    primaryDark: "##090b16",
+    secondary: "##090b16",
     success: "#31D0AA",
     warning: "#FFB237",
 };
 var brandColors = {
     binance: "#F0B90B",
+    overlay: "#452a7a",
 };
-var lightColors = __assign(__assign(__assign({}, baseColors), brandColors), { secondary: "#ffe184", background: "#FFB6C1", backgroundDisabled: "#E9EAEB", contrast: "#191326", invertedContrast: "#FFFFFF", input: "#eeeaf4", tertiary: "#EFF4F5", text: "#ffe184", textDisabled: "#BDC2C4", textSubtle: "#ffe184", borderColor: "#E9EAEB", card: "#FFC0CB", gradients: {
+var lightColors = __assign(__assign(__assign({}, baseColors), brandColors), { secondary: "##00cc98", background: "#fff", backgroundDisabled: "#E9EAEB", contrast: "#191326", invertedContrast: "#FFFFFF", input: "#eeeaf4", tertiary: "#EFF4F5", text: "##00cc98", textDisabled: "#BDC2C4", textSubtle: "##52dab7", borderColor: "#E9EAEB", card: "#fff", gradients: {
         bubblegum: "linear-gradient(139.73deg, #E6FDFF 0%, #F3EFFF 100%)",
     } });
-var darkColors = __assign(__assign(__assign({}, baseColors), brandColors), { secondary: "#ffe184", background: "#343135", backgroundDisabled: "#3c3742", contrast: "#FFFFFF", invertedContrast: "#191326", input: "#483f5a", primaryDark: "#ffe184", tertiary: "#353547", text: "#ffe184", textDisabled: "#666171", textSubtle: "#ffe184", borderColor: "#524B63", card: "#27262c", gradients: {
+var darkColors = __assign(__assign(__assign({}, baseColors), brandColors), { secondary: "#00cc98", background: "#090b16", backgroundDisabled: "#3c3742", contrast: "#FFFFFF", invertedContrast: "#191326", input: "#483f5a", primaryDark: "#00cc98", tertiary: "#353547", text: "#00cc98", textDisabled: "#666171", textSubtle: "#00cc98", borderColor: "#524B63", card: "#27262c", gradients: {
         bubblegum: "linear-gradient(139.73deg, #313D5C 0%, #3D2A54 100%)",
     } });
 
--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -1,34 +1,35 @@
 import { Colors } from "./types";
 
 export const baseColors = {
-  failure: "#ED4B9E",
-  primary: "#ffe184",
-  primaryBright: "#ffe184",
-  primaryDark: "#ffe184",
-  secondary: "#ffe184",
+  failure: "#00cc98",
+  primary: "##090b16",
+  primaryBright: "##090b16",
+  primaryDark: "##090b16",
+  secondary: "##090b16",
   success: "#31D0AA",
   warning: "#FFB237",
 };
 
 export const brandColors = {
   binance: "#F0B90B",
+  overlay: "#452a7a",
 };
 
 export const lightColors: Colors = {
   ...baseColors,
   ...brandColors,
-  secondary: "#ffe184",
-  background: "#FFB6C1",
+  secondary: "##00cc98",
+  background: "#fff",
   backgroundDisabled: "#E9EAEB",
   contrast: "#191326",
   invertedContrast: "#FFFFFF",
   input: "#eeeaf4",
   tertiary: "#EFF4F5",
-  text: "#ffe184",
+  text: "##00cc98",
   textDisabled: "#BDC2C4",
-  textSubtle: "#ffe184",
+  textSubtle: "##52dab7",
   borderColor: "#E9EAEB",
-  card: "#FFC0CB",
+  card: "#fff",
   gradients: {
     bubblegum: "linear-gradient(139.73deg, #E6FDFF 0%, #F3EFFF 100%)",
   },
@@ -37,17 +38,17 @@ export const lightColors: Colors = {
 export const darkColors: Colors = {
   ...baseColors,
   ...brandColors,
-  secondary: "#ffe184",
-  background: "#343135",
+  secondary: "#00cc98",
+  background: "#090b16",
   backgroundDisabled: "#3c3742",
   contrast: "#FFFFFF",
   invertedContrast: "#191326",
   input: "#483f5a",
-  primaryDark: "#ffe184",
+  primaryDark: "#00cc98",
   tertiary: "#353547",
-  text: "#ffe184",
+  text: "#00cc98",
   textDisabled: "#666171",
-  textSubtle: "#ffe184",
+  textSubtle: "#00cc98",
   borderColor: "#524B63",
   card: "#27262c",
   gradients: {
