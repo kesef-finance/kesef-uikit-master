4092c898143ad3fb27499ef00a13fc74d62323ae
kesef-mario <tech@kesef.finance>
2021-05-30 00:03:07 +0530

    color fix

--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -23,7 +23,7 @@ export const lightColors: Colors = {
   invertedContrast: "#FFFFFF",
   input: "#EFF4F5",
   tertiary: "#EFF4F5",
-  text: "#0098A1",
+  text: "#19202a",
   textDisabled: "#BDC2C4",
   textSubtle: "#6d6d6d",
   borderColor: "#E9EAEB",
